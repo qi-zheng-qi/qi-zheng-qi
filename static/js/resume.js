85f54fc507afedde75afbd80ef79903b122a8129
qi-zheng-qi <qi.zheng2024@hotmail.com>
2024-12-12 14:54:17 +0800

    update js file
    new function: loop images(slider)

--- static/js/resume.js
+++ static/js/resume.js
@@ -26,3 +26,51 @@
   });
 
 })(jQuery); // End of use strict
+
+class ImageSlider {
+    constructor(sliderId) {
+        this.slider = document.getElementById(sliderId);
+        this.images = this.slider.querySelectorAll('img');
+        this.index = 0;
+        this.intervalId = null;
+        this.start();
+        this.addEventListeners();
+    }
+
+    showNextImage() {
+        this.images[this.index].classList.remove('active');
+        this.index = (this.index + 1) % this.images.length;
+        this.images[this.index].classList.add('active');
+    }
+
+    start() {
+        if (!this.intervalId) {
+            this.intervalId = setInterval(() => this.showNextImage(), 2000);
+        }
+    }
+
+    pause() {
+        if (this.intervalId) {
+            clearInterval(this.intervalId);
+            this.intervalId = null;
+        }
+    }
+
+    addEventListeners() {
+        this.slider.addEventListener('mouseenter', () => this.pause());
+        this.slider.addEventListener('mouseleave', () => this.start());
+    }
+}
+
+const slider1 = new ImageSlider('slider1');
+slider1.start();
+const slider2 = new ImageSlider('slider2');
+slider2.start();
+const slider3 = new ImageSlider('slider3');
+slider3.start();
+const slider4 = new ImageSlider('slider4');
+slider4.start();
+const slider5 = new ImageSlider('slider5');
+slider5.start();
+const slider6 = new ImageSlider('slider6');
+slider6.start();
